6fac30d832791debb55093aa56b7368f016bec77
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-11 11:58:18 +0300

    2. commit.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,30 @@
-// import React from 'react';
+// import React from 'react'
+// import PropTypes from "prop-types";
+import Button from "./Button";
 
-const Header = ({title}) => {
-    return (
-        <header>
-            <h1>{title}</h1>
-        </header>
-    );
+const Header = ({ title = "Task Tracker" }) => {
+  const handleClick = () => {
+    console.log("Click with handleClick from Header");
+  };
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        color="purple"
+        text="Show Add Task Bar"
+        handleClickFromHeader={handleClick}
+      />
+    </header>
+  );
 };
 
-Header.defaultProps = {
+//DEFAULT PROPS
+// Header.defaultProps = {
+//   title: "Task Tracker",
+// };
 
-}
+// Header.propTypes = {
+//   title: PropTypes.string.isRequired,
+// };
 
 export default Header;
